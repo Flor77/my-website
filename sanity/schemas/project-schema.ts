99f241417bdfb882663b0434fffe4fb0df67c407
Flor77 <budaflorin@yahoo.com>
2023-11-19 18:00:43 +0200

    Replace all single quotes with double quotes

--- sanity/schemas/project-schema.ts
+++ sanity/schemas/project-schema.ts
@@ -1,44 +1,44 @@
 const project = {
-  name: 'project',
-  title: 'Projects',
-  type: 'document',
+  name: "project",
+  title: "Projects",
+  type: "document",
   fields: [
     {
-      name: 'name',
-      title: 'Name',
-      type: 'string'
+      name: "name",
+      title: "Name",
+      type: "string"
     },
     {
-      name: 'slug',
-      title:'Slug',
-      type:'slug',
+      name: "slug",
+      title:"Slug",
+      type:"slug",
       options:{
-        source:'name'
+        source:"name"
       }
     },
     {
-      name:'image',
-      title:'Image',
-      type:'image',
+      name:"image",
+      title:"Image",
+      type:"image",
       options:{
         hotspot:true
       },
       fields:[
-        {name:'alt',
-        title:'Alt',
-        type:'string'
+        {name:"alt",
+        title:"Alt",
+        type:"string"
       }
       ]
     },
     {
-      name:'url',
-      title:'URL',
-      type:'url'
+      name:"url",
+      title:"URL",
+      type:"url"
     },
     {
-      name:'content',
-      title:'Content',
-      type:'array',
+      name:"content",
+      title:"Content",
+      type:"array",
       of:[{type: "block"}],
     }
   ]
